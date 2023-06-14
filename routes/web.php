13fbb7e963f3a7cc96767ce82253ac5089c7e14f
raffaele-catalano <raf.catalano@gmail.com>
2023-06-14 17:56:44 +0200

    update route->home + imported 2 controllers & Artist Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArtistController;
+use App\Models\Artist;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
