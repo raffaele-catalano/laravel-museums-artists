35e81fbf129e4b8c419f58e7465a409e1bab3edc
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-14 16:25:21 +0200

    add migrate museum

---
+++ database/migrations/2023_06_14_140608_create_museums_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('museums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('slug', 50)->unique();
+            $table->string('nation', 20)->nullable();
+            $table->string('city', 20)->nullable();
+            $table->string('address', 20);
+            $table->string('web_site', 50)->nullable();
+            $table->text('photo')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 10, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('museums');
+    }
+};
