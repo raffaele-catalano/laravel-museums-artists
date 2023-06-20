4ea0933f13a0bc443af873d4335ae70c7fa8b348
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-20 16:21:07 +0200

    add artist.show

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -47,7 +47,7 @@ class ArtistController extends Controller
      */
     public function show(Artist $artist)
     {
-        //
+        return view('artists.show', compact('artist'));
     }
 
     /**
